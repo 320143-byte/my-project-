e52a874fb05dda50094684f3acfba27ba892e297
320143-byte <320143@hkis.edu.hk>
2026-06-08 13:34:46 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,59 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Godzilla":return img`
+........................
+........................
+..............8.........
+........88...8888888....
+........88888855555888..
+........888855555555888.
+.....88.888555555555588.
+.....8888555555321555588
+......8855555555ff55d558
+...888855555555555555558
+...8888555dd5555bb13bb88
+...888855ddddd555b33358.
+....88dd5ddddddd55b3358.
+.....8ddddb55bdddd55558.
+..8888ddddb55bbbbbccc88.
+.888888ddddb5555cb8888..
+.8d8888dddddc555cb8558..
+.2dd8ddddddddcccbb858...
+.2888dd8dd55dbbbb888....
+.222ddd8dd555dbbb888....
+..2222d8888555bb888.....
+....2288888d555888......
+......28888888888.......
+........8.....8.d.......
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
